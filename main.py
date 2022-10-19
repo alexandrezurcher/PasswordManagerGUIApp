7d2b86490f3a51f996297d4ea001fbc5078a5c2f
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-19 07:48:50 +0100

    grid layout created

--- main.py
+++ main.py
@@ -7,11 +7,35 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="./logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
+website_label = Label(text="Website:")
+website_label.grid(column=0,row=1)
+
+username_label = Label(text="Email/username:")
+username_label.grid(column=0,row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0,row=3)
+
+website_entry = Entry(width=55)
+website_entry.grid(column=1,row=1,columnspan=2)
+
+user_entry = Entry(width=55)
+user_entry.grid(column=1,row=2,columnspan=2)
+
+password_entry = Entry(width=34)
+password_entry.grid(column=1,row=3)
+
+generate_password_button = Button(text="Generate Password",width=17)
+generate_password_button.grid(column=2,row=3)
+#generate_password_button.place(x=190, y=240)
+
+add_button = Button(text="Add", width=40)
+add_button.grid(column=1,row=4,columnspan=2)
 
 window.mainloop()
