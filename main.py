90813c711ac002a8f22f3220f742f89eeb9a6466
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-21 08:14:40 +0100

    catch FileNotFoundError

--- main.py
+++ main.py
@@ -46,15 +46,20 @@ def save():
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPasswo"
                                                               f"rd: {password} \nDo you want to proceed and save it?")
         if is_ok:
-            with open("data.json", "r") as data_file:
-                # Reading old data
-                data = json.load(data_file)
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
                 # Update old data with new data
                 data.update(new_data)
-            with open("data.json", "w") as data_file:
-                # Saving updated data
-                json.dump(data, data_file, indent=4)
-
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
